9d3728e88910f890d5210b00991f7da9d5723392
Jörg Jooss <joerg@joergjooss.de>
2016-09-30 22:33:11 +0200

    Updated to Angular 2 final release

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,5 +1,5 @@
 /**
- * System configuration for Angular 2 samples
+ * System configuration for Angular samples
  * Adjust as necessary for your application needs.
  */
 (function (global) {
@@ -12,6 +12,7 @@
     map: {
       // our app is within the app folder
       app: 'app',
+
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -21,18 +22,10 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      // angular testing umd bundles
-      '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
-      '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
-      '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
-      '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
-      '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
-      '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
-      '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
-      '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js',
+
       // other libraries
-      'rxjs':                       'npm:rxjs',
-      'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api'
+      'rxjs':                      'npm:rxjs',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -43,7 +36,7 @@
       rxjs: {
         defaultExtension: 'js'
       },
-      'angular2-in-memory-web-api': {
+      'angular-in-memory-web-api': {
         main: './index.js',
         defaultExtension: 'js'
       }
